0fd56828cfc9018ee7512cae5b20317b15dc7ded
Edward A Maxedon, Sr <edadma@vinctus.com>
2019-10-11 20:58:47 -0400

    code subscribe

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,30 +2,69 @@ package xyz.hyperreal.stomp_server
 
 import typings.node.httpMod
 import typings.node.httpMod.{ClientRequest, ServerResponse}
-import typings.uuid
+import typings.uuid.uuidMod
 import typings.sockjs
-import typings.sockjs.sockjsMod.ServerOptions
+import typings.sockjs.sockjsMod.{Connection, ServerOptions}
 import typings.sockjs.sockjsStrings
 
 import scala.scalajs.js
 import scala.scalajs.js.RegExp
+import collection.mutable
 
 
 object Main extends App {
 
-  val sockjs_echo = sockjs.sockjsMod.createServer()
+  def authorize( token: String ) = {
+    true
+  }
+
+  val server = new StompServer( "ShuttleControl/1.0", authorize, true )
+
+}
+
+class StompServer( name: String, authorize: String => Boolean, debug: Boolean = false ) {
+
+  case class SubscriptionKey( removeAddress: String, remotePort: Double, id: String )
+  case class Subscription( destination: String, ack: String )
+
+  private val sockjs_echo = sockjs.sockjsMod.createServer()
+  private val subscriptions = new mutable.HashMap[SubscriptionKey, Subscription]
 
   sockjs_echo.on_connection( sockjsStrings.connection, conn => {
-    println("connection")
+    dbg( s"sockjs connection: ${conn.remoteAddress}, ${conn.remotePort}, ${conn.url}" )
     conn.on( "data", (message: String) => {
       parseMessage( message ) match {
         case ("CONNECT", headers, _) =>
-          println( s"connecting ${headers("login")}" )
+          dbg( s"stomp connection: $headers" )
+
+          if (headers get "Authorization" match {
+            case None => true
+            case Some( token ) => authorize( token )
+          }) {
+            sendMessage(conn, "CONNECTED",
+              List(
+                "version" -> "1.2",
+                "heart-beat" -> "10000,10000",
+                "session" -> createSession,
+                "server" -> name))
+          } else {
+            dbg( s"*** not authorized" )
+          }
+        case ("SUBSCRIBE", headers, _) =>
+          val key = SubscriptionKey( conn.remoteAddress, conn.remotePort, headers("id") )
+
+          dbg( s"subscribe: $headers" )
+          subscriptions get key match {
+            case Some( _ ) =>
+              dbg( s"*** subscription duplicate: $key" )
+            case None =>
+              subscriptions(key) = Subscription( headers("destination"), headers.getOrElse("ack", "auto") )
+          }
       }
     } )
   } )
 
-  val server = httpMod.createServer()
+  private val server = httpMod.createServer()
 
   server.addListener("upgrade", (reqres: js.Any) => {
     println( "upgrade" )
@@ -34,12 +73,18 @@ object Main extends App {
 
   sockjs_echo.installHandlers( server, js.Dynamic.literal(prefix = "/ws").asInstanceOf[ServerOptions] )
   println(" [*] Listening on 0.0.0.0:15674")
-  //server.listen( 15674, "0.0.0.0" )
+  server.listen( 15674, "0.0.0.0" )
 
-  def buildMessage( command: String, headers: List[(String, String)], body: String ) = {
+  private def sendMessage( conn: Connection, command: String, headers: List[(String, String)], body: String = "" ) = {
     val escaped = headers map {case (k, v) => s"${escape(k)}:${escape( v )}"} mkString "\n"
 
-    s"$command\n$escaped\n\n$body\u0000"
+    conn.write( s"$command\n$escaped\n\n${body}\u0000" )
+  }
+
+  private def createSession = {
+    val id = uuidMod.^.v4
+
+    id
   }
 
   private def escape( s: String ) = s.
@@ -48,24 +93,32 @@ object Main extends App {
     replace( "\n", "\\n" ).
     replace( ":", "\\c" )
 
-}
+  private def dbg( s: String ) =
+    if (debug)
+      println( s"DEBUG $s" )
 
-object parseMessage extends {
+  //def send( destination: )
 
-  private val stompMessageRegex = RegExp( """([A-Z]+)\r?\n(.*?)\r?\n\r?\n([^\00]*)\00(?:\r?\n)*""", "s" )
-  private val HeaderRegex = """([a-z0-9\]+):(.+)"""r
+  object parseMessage extends {
 
-  def apply( message: String ) = {
-    val List(_, command, headers, body ) = stompMessageRegex.exec( message ).toList
-    val headerMap = HeaderRegex findAllMatchIn headers.toString map (m => unescape( m.group(1) ) -> unescape( m.group(2) )) toMap
+    private val stompMessageRegex = RegExp( """([A-Z]+)\r?\n(.*?)\r?\n\r?\n([^\00]*)\00(?:\r?\n)*""", "s" )
+    private val HeaderRegex = """([a-z0-9\\]+):(.+)"""r
 
-    (command.toString, headerMap, body.toString)
+    def apply( message: String ) = {
+      dbg( s"parseMessage: ${escape(message)}" )
+
+      val List(_, command, headers, body ) = stompMessageRegex.exec( message ).toList
+      val headerMap = HeaderRegex findAllMatchIn headers.toString map (m => unescape( m.group(1) ) -> unescape( m.group(2) )) toMap
+
+      (command.toString, headerMap, body.toString)
+    }
+
+    private def unescape( s: String ) = s.
+      replace( "\\r", "\r" ).
+      replace( "\\n", "\n" ).
+      replace( "\\c", ":" ).
+      replace( "\\\\", "\\" )
+
   }
-
-  private def unescape( s: String ) = s.
-    replace( "\\r", "\r" ).
-    replace( "\\n", "\n" ).
-    replace( "\\c", ":" ).
-    replace( "\\\\", "\\" )
 
 }
